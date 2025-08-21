5d92caba5f02b077293f6901d3200c061db91260
ravvitkim <mirkim6086@naver.com>
2025-08-21 15:59:29 +0900

    날짜 시간 관련 작업 1.after,before는 자기값 제외 2.between은 포함 정리

---
+++ src/test/java/com/my/jpaTest/repository/UsersRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.my.jpaTest.repository;
+
+import com.my.jpaTest.dto.Gender;
+import net.bytebuddy.build.ToStringPlugin;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class UsersRepositoryTest {
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Test
+    @DisplayName("1.findByName Test")
+    void findByNameTest() {
+        String name = "Wyatt Hunstone";
+        usersRepository
+                .findByName(name)
+                .stream().forEach(x -> System.out.println(x));
+    }
+
+    @Test
+    @DisplayName("2.pink색상 상위3개 찾기 테스트")
+    void findByColor() {
+        String color = "Pink";
+        usersRepository
+                .findTop3ByLikeColor(color)
+                .forEach(x -> System.out.println(x));
+    }
+
+    @Test
+    @DisplayName("findByGenderAndLikeColor_test")
+    void findByGenderAndLikeColor() {
+        usersRepository.findByGenderAndLikeColor(Gender.Female, "Red")
+                .forEach(x -> System.out.println(x));
+    }
+
+    @Test
+    @DisplayName("findByCreatedAtAfter_test")
+    void findByCreatedAtAfter() {
+        LocalDate yesterday = LocalDate.now().minusDays(2L);
+        LocalDateTime start = yesterday.atTime(23, 59, 59);
+        usersRepository.findByCreatedAtAfter(start)
+                .forEach(x -> System.out.println(x));
+    }
+
+    @Test
+    @DisplayName("최근 한달 자료 찾기 (오늘포함)")
+    void findByCreatedAtBetween() {
+//        한달 이전의 기준일 설정
+        LocalDate baseDate = LocalDate.now().minusMonths(1L);
+//        한달 전 날에다 시분초를 붙인다.
+        LocalDateTime start = baseDate.atTime(0, 0, 0);
+        LocalDateTime end = LocalDateTime.now();
+        usersRepository.findByCreatedAtBetween(start,end)
+                .forEach(x-> System.out.println(x));
+    }
+}
